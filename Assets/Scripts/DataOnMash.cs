01ad806d2d5dd85c3016a3b7d43b9d0727caaf92
Antaresmag <kunderenko2@gmail.com>
2020-10-10 13:31:13 +0300

    first commit

---
+++ Assets/Scripts/DataOnMash.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
+
+public class DataOnMash : MonoBehaviour
+{
+    public static bool createObj = false;
+    public int countClick = 0;
+    public Text clickText;
+    public GameObject cube;
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cube.GetComponent<GameObject>();
+        clickText.text = clickText.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            //CreateMesh();
+            Debug.Log("Click");
+            RaycastHit hit;
+
+           
+            
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray,out hit))
+            {
+                if(hit.transform.name == "Zone" && createObj == false)
+                {
+                    
+                    Instantiate(cube, hit.point, Quaternion.identity);
+                    Debug.Log("Creat obj");
+                    createObj = true;
+
+                }
+                else if(hit.transform.name == "Cube" || hit.transform.name == "Cube(Clone)")
+                {
+                    Debug.Log("Hit in Cube");
+                    countClick++;
+                    clickText.text = Mathf.Round(countClick).ToString();
+                    Debug.Log(countClick);
+                    
+                }
+                else
+                {
+                    Debug.Log("miss");
+                }
+            }
+        }
+        
+    }
+
+    
+
+    //void CreateMesh()
+    //{
+    //    Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //    cube.CreatePrimitive(CurrentPrimitive).transform.Translate(mRay.origin + (mRay.direction * primDistance));
+    //    //Instantiate(cube, Input.mousePosition));
+    //}
+}
---
+++ Assets/Scripts/SwitchColor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using UnityEngine.UI;
+
+public class SwitchColor : MonoBehaviour
+{
+    public float timeStart = 5;
+    public Text timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText.text = timeStart.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DataOnMash.createObj)
+        {
+            timeStart -= Time.deltaTime;
+            timerText.text = Mathf.Round(timeStart).ToString();
+
+            if (timeStart <= 0)
+            {
+                //timeStart = 5;
+                ChangeColor();
+            }
+        }
+    }
+
+    void ChangeColor()
+    {
+        if (name == "Cube(Clone)")
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
+            //Thread.Sleep(750);
+            timeStart = 5;
+        }
+
+    }
+
+}
